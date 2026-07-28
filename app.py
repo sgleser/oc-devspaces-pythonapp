3a02de526517bb22eab37766f3158ebf7404cce1
Szabolcs Gleszer <sgleszer@sgleszer-thinkpadx1carbongen9.rmtat.csb>
2026-07-29 00:33:38 +0200

    OpenShift manifests

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return os.environ.get('APP_MESSAGE', 'Hello World!')
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
